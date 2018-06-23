342613667b526a74affddc6f7b8aa1e6a6782bb0
GuihAntunes <antunesg.10@gmail.com>
2018-06-23 11:07:45 -0300

    Including lottie and splash screen

--- HackathonChallenge/HackathonChallenge/Source/AppDelegate.swift
+++ HackathonChallenge/HackathonChallenge/Source/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        
+        GMSPlacesClient.provideAPIKey("AIzaSyCI01YR6_ddftHySBhB6cqTC54fXt_wwmQ")
         
         return true
     }
